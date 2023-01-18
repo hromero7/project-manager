8ff9d8d2719dbc2c700f310120ea6fdd8deba753
Marc S <marcsalaver@gmail.com>
2023-01-18 01:48:18 -0800

    finished reviewing, moving on to member functionality within project. starting with adding members to project

--- client/src/Components/TaskAssignee/TaskAssignee.js
+++ client/src/Components/TaskAssignee/TaskAssignee.js
@@ -4,218 +4,146 @@ import { Form, Dropdown } from "react-bootstrap";
 import TaskAPI from "../../Utils/TaskAPI";
 
 const TaskAssignee = (props) => {
-    const [value, setValue] = useState("");
-    const [getSearchData, setGetSearchData] = useState(false);
-    const [show, setShow] = useState(false);
-    const [searchList, setSearchList] = useState("");
-    const [checked, setChecked] = useState([]);
+  const [value, setValue] = useState("");
+  const [getSearchData, setGetSearchData] = useState(false);
+  const [show, setShow] = useState(false);
+  const [searchList, setSearchList] = useState("");
+  const [checked, setChecked] = useState([]);
 
-    useEffect(() => {
-      console.log(searchList);
-      console.log(checked)
-      if(searchList.length > 1 && props.assignee.length > 1) {
-        for(let i = 0; i < searchList.length; i++) {
-          const verifyChecked = props.assignee.find((user) =>
-            user.username === searchList[i].username);
-            if(verifyChecked) checked[i] = true;
-            console.log(checked[i], i);
-        }
-      // const verifyChecked = searchList.map((user, i) => 
-      //   props.assignee[i].username === user.username);
-      //   if (verifyChecked) setChecked(verifyChecked);
-      } else if (searchList.length === 1){
-        const verifyChecked = props.assignee.find((user) =>
-        user.username === searchList[0].username);
-        // console.log(verifyChecked, "line 24")
-        if (verifyChecked) setChecked([true]);
-        console.log(checked);
-      }
-      //setChecked(new Array(searchList.length).fill(false));
-    }, [searchList])
-
-    // const verifyChecked = (e) => {
-    //   e.preventDefault()
-    //   console.log("e.target")
-    //   const user = props.assignee.find((user) => 
-    //     e.target.attributes["username"].value === user.username
-    //   )
-    //   if (user) return true;
-    //   else return false
-    // }
-
-    const handleAssignTask = async (e, index) => {
-      console.log(checked);
-        // const checked = e.target.checked;
-        // const state = checked.find(check => e.target.attributes["username"].value === check[e.target.attributes["username"].value].username)
-        // if (state) {
-        //   checked[state] = { username: state.username, isChecked: true };
-        // }
-     
-          // setChecked({...checked}, {username: e.target.attributes["username"].value, isChecked: true});
-        const user = { 
-                    id: e.target.attributes["userId"].value, 
-                    username : e.target.attributes["username"].value
-                  }
-        if (!checked[index]) {
-          const res = await TaskAPI.addAssignee(props.projectId, props.taskId, user);
-          const updatedChecked = checked.map((item, i) =>
-            i === index ? !item : item
-          );
-          setChecked(updatedChecked);
-          console.log(res);
-        } else {
-          const userId = e.target.attributes["userId"].value
-          const res = await TaskAPI.removeAssignee(props.projectId, props.taskId, userId);
-          // const updatedChecked = checked.filter((item, i) =>
-          //   i === index
-          // );
-          const updatedChecked = checked.map((item, i) =>
-            i === index ? !item : item
-          );
-          setChecked(updatedChecked);
-          console.log(res);
-
-        }
-        props.getProjectData();
-    }
-
-    //const handleRemoveAssignee = async(e) => {
-      // console.log(e.target.checked)
-      // const state = checked.find(check => e.target.attributes["username"].value === check[e.target.attributes["username"].value].username)
-      // checked[state] = { username: state.username, isChecked: false };
-      // const userId = e.target.attributes["userId"].value
-      // const res = await TaskAPI.removeAssignee(props.projectId, props.taskId, userId);
-      //   console.log(res);
-      //   props.getProjectData();
-    //}
-
-    const CustomUserAddToggle = React.forwardRef(({ children, onClick }, ref) => (
-        <a 
-            href=""
-            ref={ref}
-            onClick={(e) => {
-            e.preventDefault();
-            onClick(e);
-            }}>
-            {children} 
-        </a>
-        ));
-
-        const taskMenu = React.forwardRef(
-        ({ children, style, className, "aria-labelledby": labeledBy }, ref) => {
-            return (
-            <div
-                ref={ref}
-                style={style}
-                className={className}
-                aria-labelledby={labeledBy}
-            >
-                <ul className="list-unstyled"></ul>
-                {children}
-            </div>
-            );
-        }
+  useEffect(() => {
+    if (searchList.length > 1 && props.assignee.length > 1) {
+      for (let i = 0; i < searchList.length; i++) {
+        const verifyChecked = props.assignee.find(
+          (user) => user.username === searchList[i].username
         );
-    return (
-        <Dropdown
-        autoClose="outside"
-        // onToggle={() => setOpen(!open)}
-        // show={open}
+        if (verifyChecked) checked[i] = true;
+        console.log(checked[i], i);
+      }
+    } else if (searchList.length === 1) {
+      const verifyChecked = props.assignee.find(
+        (user) => user.username === searchList[0].username
+      );
+      if (verifyChecked) setChecked([true]);
+      console.log(checked);
+    }
+  }, [searchList]);
+
+  const handleAssignTask = async (e, index) => {
+    const user = {
+      id: e.target.attributes["userId"].value,
+      username: e.target.attributes["username"].value,
+    };
+    if (!checked[index]) {
+      const res = await TaskAPI.addAssignee(
+        props.projectId,
+        props.taskId,
+        user
+      );
+      const updatedChecked = checked.map((item, i) =>
+        i === index ? !item : item
+      );
+      setChecked(updatedChecked);
+      console.log(res);
+    } else {
+      const userId = e.target.attributes["userId"].value;
+      const res = await TaskAPI.removeAssignee(
+        props.projectId,
+        props.taskId,
+        userId
+      );
+
+      const updatedChecked = checked.map((item, i) =>
+        i === index ? !item : item
+      );
+      setChecked(updatedChecked);
+      console.log(res);
+    }
+    props.getProjectData();
+  };
+
+  const CustomUserAddToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+    </a>
+  ));
+
+  const taskMenu = React.forwardRef(
+    ({ children, style, className, "aria-labelledby": labeledBy }, ref) => {
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
         >
-          <Dropdown.Toggle
-            as={CustomUserAddToggle}
-            id="dropdown-custom-components"
-          >
-             <i className="fa-solid fa-circle-plus"></i>
-          </Dropdown.Toggle>
+          <ul className="list-unstyled"></ul>
+          {children}
+        </div>
+      );
+    }
+  );
+  return (
+    <Dropdown autoClose="outside">
+      <Dropdown.Toggle as={CustomUserAddToggle} id="dropdown-custom-components">
+        <i className="fa-solid fa-circle-plus"></i>
+      </Dropdown.Toggle>
 
-          <Dropdown.Menu as={taskMenu}>
-            <Dropdown.Header>
-              Add a member:
-            </Dropdown.Header>
+      <Dropdown.Menu as={taskMenu}>
+        <Dropdown.Header>Add a member:</Dropdown.Header>
+        <Form.Control
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Search to add..."
+          autoComplete="off"
+          onChange={(e) => {
+            if (e.target.value.length <= 1) {
+              setGetSearchData(false);
+            } else {
+              axios
+                .get(`/api/user/finduser/${e.target.value}`)
+                .then((res) => {
+                  setSearchList(res.data);
+                  setGetSearchData(true);
+                })
+                .catch((err) => console.log("err: ", err));
+            }
 
-            <Form.Control
-              autoFocus
-              className="mx-3 my-2 w-auto"
-              placeholder="Search to add..."
-              autoComplete="off"
-              onChange={(e) => {
-                if (e.target.value.length <= 1) {
-                  setGetSearchData(false);
-                } else {
-                  axios
-                    .get(
-                      `/api/user/finduser/${e.target.value}`
-                    )
-                    .then((res) => {
-                      // console.log("res: ", res.data);
-                      setSearchList(res.data);
-                      setGetSearchData(true);
-                    })
-                    .catch((err) =>
-                      console.log("err: ", err)
-                    );
-                }
-
-                setValue(e.target.value);
-              }}
-              value={value}
-            />
-            <Dropdown.Divider />
-            {getSearchData ? (
-              searchList.map((searchItem, i) => {
-                return (
-                  <Dropdown.Item as="form" key={searchItem._id}>
-                    <Form>
-                      {/* {
-                        props.assignee.find((user) => 
-                        searchItem.username === user.username)? 
-                        <Form.Check 
-                        type="switch"
-                        //checked={checked[searchItem.username]? checked[searchItem.username].isChecked : ""}
-                        id="custom-switch"
-                        username={searchItem.username}
-                        userId={searchItem._id}
-                        onClick={handleRemoveAssignee}
-                        label={searchItem.username}
-                      ></Form.Check> :
-                      <Form.Check 
-                        type="switch"
-                        checked={checked[i]}
-                        id="custom-switch"
-                        username={searchItem.username}
-                        userId={searchItem._id}
-                        onChange={() => handleAssignTask(i)}
-                        label={searchItem.username}
-                      ></Form.Check>
-                      } */}
-                      <Form.Check 
-                        type="switch"
-                        checked={checked[i]}
-                        id="custom-switch"
-                        username={searchItem.username}
-                        userId={searchItem._id}
-                        onChange={(e) => handleAssignTask(e, i)}
-                        label={searchItem.username}
-                      ></Form.Check>
-                    </Form>
-                  </Dropdown.Item>
-                );
-              })
-            ) : (
-              <Dropdown.Item
-                eventKey="1"
-                // onClick={console.log(
-                //   "searchList",
-                //   searchList
-                // )}
-              >
-                List All Members  
+            setValue(e.target.value);
+          }}
+          value={value}
+        />
+        <Dropdown.Divider />
+        {getSearchData ? (
+          searchList.map((searchItem, i) => {
+            return (
+              <Dropdown.Item as="form" key={searchItem._id}>
+                <Form>
+                  <Form.Check
+                    type="switch"
+                    checked={checked[i]}
+                    id="custom-switch"
+                    username={searchItem.username}
+                    userId={searchItem._id}
+                    onChange={(e) => handleAssignTask(e, i)}
+                    label={searchItem.username}
+                  ></Form.Check>
+                </Form>
               </Dropdown.Item>
-            )}
-          </Dropdown.Menu>
-        </Dropdown>
-    )
-}
+            );
+          })
+        ) : (
+          <Dropdown.Item eventKey="1">List All Members</Dropdown.Item>
+        )}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
 export default TaskAssignee;
--- client/src/Pages/TaskPage/TaskPage.js
+++ client/src/Pages/TaskPage/TaskPage.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Table, Button } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  Button,
+  Dropdown,
+  Form,
+} from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import ProjectAPI from "../../Utils/ProjectAPI";
 import { AuthContext } from "../../Context/AuthContext";
@@ -7,84 +15,161 @@ import TaskModal from "../../Components/TaskModal/TaskModal";
 import TaskActionMenu from "../../Components/TaskActionMenu/TaskActionMenu";
 import "./TaskPage.css";
 import TaskAssignee from "../../Components/TaskAssignee/TaskAssignee";
+import axios from "axios";
 
 const TaskPage = () => {
-    const { ID } = useParams();
-    const [projectData, setProjectData] = useState({
-        date: "",
-        members: [],
-        tasks: [],
-        title: "",
-        userId: "",
-        id: ""
+  const { ID } = useParams();
+  const [projectData, setProjectData] = useState({
+    date: "",
+    members: [],
+    tasks: [],
+    title: "",
+    userId: "",
+    id: "",
+  });
+  const [value, setValue] = useState();
+  const [getSearchData, setGetSearchData] = useState();
+  const [searchList, setSearchList] = useState([]);
+
+  useEffect(() => {
+    getProjData();
+  }, []);
+
+  const getProjData = () => {
+    ProjectAPI.getOneProject(ID).then((response) => {
+      // console.log("line 27", response)
+      setProjectData({
+        date: response.date,
+        members: response.members,
+        tasks: response.tasks,
+        title: response.title,
+        userId: response.userId,
+        id: response._id,
+      });
     });
+  };
+  return (
+    <Container className="task-container">
+      <Row>
+        <Col>
+          <Row>
+            <Col>
+              <h1>{projectData.title}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <TaskModal projectId={ID} getProjData={getProjData} />
+            </Col>
+            <Col>
+              <Dropdown autoClose="outside">
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Add members:
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">
+                    <Dropdown.Header>Add members:</Dropdown.Header>
+                    <Form>
+                      <Form.Control
+                        autoFocus
+                        className="mx-3 my-2 w-auto"
+                        placeholder="Search to add..."
+                        autoComplete="off"
+                        onChange={(e) => {
+                          if (e.target.value.length <= 1) {
+                            setSearchList([]);
+                            setGetSearchData(false);
+                          } else {
+                            axios
+                              .get(`/api/user/finduser/${e.target.value}`)
+                              .then((res) => {
+                                setSearchList(res.data);
+                                setGetSearchData(true);
+                              })
+                              .catch((err) => console.log("err: ", err));
+                          }
+                          console.log("searchList: ", searchList);
+                          setValue(e.target.value);
+                        }}
+                        value={value}
+                      />
+                    </Form>
+                  </Dropdown.Item>
+                  {searchList.map((item) => {
+                    return (
+                      <Dropdown.Item href="#/action-2">
+                        {item.firstName}
+                      </Dropdown.Item>
+                    );
+                  })}
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Members:</Dropdown.Header>
+                  <Dropdown.Item href="#/action-3">Member pool</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+          </Row>
 
-    useEffect(() => {
-        getProjData();
-      }, []);
-
-
-    const getProjData = () => {
-        ProjectAPI.getOneProject(ID).then((response) => {
-            console.log("line 27", response)
-            setProjectData({
-            date: response.date,
-            members: response.members,
-            tasks: response.tasks,
-            title: response.title,
-            userId: response.userId,
-            id: response._id
-            });
-        });
-    };
-    return (
-    <div className="task-container">
-        <h1 style={{"color": "black"}}>{projectData.title}</h1>
-        <TaskModal projectId={ID} getProjData={getProjData}/>
-        <Table striped hover responsive>
-        <thead>
-            <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Status</th>
-            <th>Assignee</th>
-            <th>Due date</th>
-            <th>Priority</th>
-            <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {projectData.tasks.map((task, i) => {
+          <Table striped hover responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Assignee</th>
+                <th>Due date</th>
+                <th>Priority</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {projectData.tasks.map((task, i) => {
                 return (
-                    <tr key={i}>
-                        <td>{i + 1}</td>
-                        <td>{task.taskTitle}</td>
-                        <td>{task.status}</td>
-                        <td>{task.assignee.map(assignee => {
-                            return (
-                                <div>{assignee.username}</div>
-                            )
-                        })}
-                            <TaskAssignee projectId={projectData.id} taskId={task._id} assignee={task.assignee} getProjectData={getProjData}/>
-                        </td>
-                        <td>{task.dueDate}</td>
-                        <td>
-                            <span className={`task-priority ${task.priority === "High"? "priority-high" : 
-                                task.priority === "Medium"? "priority-medium" : 
-                                "priority-low"}`}>
-                                    {task.priority}
-                            </span>
-                        </td>
-                        <td>
-                            <TaskActionMenu projectId={projectData.id} taskId={task._id} getProjectData={getProjData}/>
-                        </td>
-                    </tr>
-                )
-            })}
-        </tbody>
-        </Table>
-    </div>
-    )
-}
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td>{task.taskTitle}</td>
+                    <td>{task.status}</td>
+                    <td>
+                      {task.assignee.map((assignee) => {
+                        return <div>{assignee.username}</div>;
+                      })}
+                      <TaskAssignee
+                        projectId={projectData.id}
+                        taskId={task._id}
+                        assignee={task.assignee}
+                        getProjectData={getProjData}
+                      />
+                    </td>
+                    <td>{task.dueDate}</td>
+                    <td>
+                      <span
+                        className={`task-priority ${
+                          task.priority === "High"
+                            ? "priority-high"
+                            : task.priority === "Medium"
+                            ? "priority-medium"
+                            : "priority-low"
+                        }`}
+                      >
+                        {task.priority}
+                      </span>
+                    </td>
+                    <td>
+                      <TaskActionMenu
+                        projectId={projectData.id}
+                        taskId={task._id}
+                        getProjectData={getProjData}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default TaskPage;
