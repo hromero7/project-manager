a6c0df367f213f2b5bf95d442ec5886271f61577
Marc S <marcsalaver@gmail.com>
2023-02-14 11:11:34 -0800

    not sure what broke

--- client/src/Components/Assets/RisingBubbles/RisingBubbles.js
+++ client/src/Components/Assets/RisingBubbles/RisingBubbles.js
@@ -5,8 +5,8 @@ import "./RisingBubbles.css";
 export default function RisingBubbles() {
   return (
     <Col>
-      <div class="area">
-        <ul class="circles">
+      <div className="area">
+        <ul className="circles">
           <li></li>
           <li></li>
           <li></li>
