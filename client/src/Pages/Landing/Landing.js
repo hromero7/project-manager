139c78a2a3620fd28b0c01d526ed4547175714c8
Marc S <marcsalaver@gmail.com>
2023-02-16 13:50:33 -0800

    landingcss work. linked images

--- client/src/Pages/Landing/Landing.js
+++ client/src/Pages/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import Logo from "../../Components/Assets/3rd logo/Logo";
 import RisingBubbles from "../../Components/Assets/RisingBubbles/RisingBubbles";
 import NewUser from "../../Components/Loginform/NewUser/NewUser";
@@ -59,13 +59,13 @@ const LandingPage = () => {
       <Container className="ContentContainer landing" fluid>
         <Container className="productDescriptionCont landing">
           <Row className="productDescription">
-            <Col className="productDescriptionIcon">
+            {/* <Col className="productDescriptionIcon">
               <span className="pdIconCont">
                 <span className="pdIconInner">
                   <i className=" fa-solid fa-bell pdIcon"></i>
                 </span>
               </span>
-            </Col>
+            </Col> */}
             <Col className="productDescriptionText">
               <h4>Stay informed, no matter where you are</h4>
               <p>
@@ -78,17 +78,27 @@ const LandingPage = () => {
               </p>
             </Col>
           </Row>
+          <Container className="landingImageCont" fluid>
+            <Row className="landingImageRow">
+              <Col className="landingImageCol">
+                <Image
+                  className="LandingImage3"
+                  src="https://images.unsplash.com/photo-1521737451536-00a86f630f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjI0fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+              </Col>
+            </Row>
+          </Container>
         </Container>
 
         <Container className="productDescriptionCont">
           <Row className="productDescription">
-            <Col className="productDescriptionIcon">
+            {/* <Col className="productDescriptionIcon">
               <span className="pdIconCont">
                 <span className="pdIconInner">
                   <i className=" fa-solid fa-users pdIcon"></i>
                 </span>
               </span>
-            </Col>
+            </Col> */}
             <Col className="productDescriptionText">
               <p>
                 Whether you're working with a small group or a large team, 3rd
@@ -100,17 +110,27 @@ const LandingPage = () => {
               </p>
             </Col>
           </Row>
+          <Container className="landingImageCont" fluid>
+            <Row className="landingImageRow">
+              <Col className="landingImageCol">
+                <Image
+                  className="LandingImage3"
+                  src="https://images.unsplash.com/photo-1577962917302-cd874c4e31d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU5fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+              </Col>
+            </Row>
+          </Container>
         </Container>
 
         <Container className="productDescriptionCont">
           <Row className="productDescription">
-            <Col className="productDescriptionIcon">
+            {/* <Col className="productDescriptionIcon">
               <span className="pdIconCont">
                 <span className="pdIconInner">
                   <i className="fa-solid fa-comment pdIcon"></i>
                 </span>
               </span>
-            </Col>
+            </Col> */}
             <Col className="productDescriptionText">
               <p>
                 Get the right information, at the right time, with 3rd's
@@ -124,6 +144,16 @@ const LandingPage = () => {
               </p>
             </Col>
           </Row>
+          <Container className="landingImageCont" fluid>
+            <Row className="landingImageRow">
+              <Col className="landingImageCol">
+                <Image
+                  className="LandingImage3"
+                  src="https://images.unsplash.com/photo-1623303647440-967d26b95b47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzAwfHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+              </Col>
+            </Row>
+          </Container>
         </Container>
       </Container>
     </Container>
