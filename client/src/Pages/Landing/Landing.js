b433346dd9b70be65a08164cdee05ca06e8c2899
Marc S <marcsalaver@gmail.com>
2023-02-17 12:27:23 -0800

    image container still needs proper formatting

--- client/src/Pages/Landing/Landing.js
+++ client/src/Pages/Landing/Landing.js
@@ -55,18 +55,11 @@ const LandingPage = () => {
         </Container>
         <RisingBubbles />
       </Container>
-
+      <Container></Container>
       <Container className="ContentContainer landing" fluid>
         <Container className="productDescriptionCont ">
-          <Container className="productDescription">
-            <Row>
-              {/* <Col className="productDescriptionIcon">
-              <span className="pdIconCont">
-                <span className="pdIconInner">
-                  <i className=" fa-solid fa-bell pdIcon"></i>
-                </span>
-              </span>
-            </Col> */}
+          <Container className="descImageCont">
+            <Container className="productDescription">
               <Col className="productDescriptionText">
                 <h4>Stay informed, no matter where you are</h4>
                 <p>
@@ -78,89 +71,77 @@ const LandingPage = () => {
                   on what matters most.
                 </p>
               </Col>
-            </Row>
-          </Container>
-          <Container className="landingImageCont" fluid>
-            <Row className="landingImageRow">
-              <Col className="landingImageCol">
-                <Image
-                  className="LandingImage"
-                  src="https://images.unsplash.com/photo-1521737451536-00a86f630f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjI0fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                />
-              </Col>
-            </Row>
-          </Container>
-        </Container>
-
-        <Container className="productDescriptionCont">
-          <Container className="productDescription">
-            <Row>
-              {/* <Col className="productDescriptionIcon">
-              <span className="pdIconCont">
-                <span className="pdIconInner">
-                  <i className=" fa-solid fa-users pdIcon"></i>
-                </span>
-              </span>
-            </Col> */}
-              <Col className="productDescriptionText">
-                <h4>
-                  Whether you're working with a small group or a large team
-                </h4>
-                <p>
-                  3rd has the tools you need to streamline communication, assign
-                  tasks, and get things done. Say goodbye to endless emails and
-                  confusion, and hello to a more efficient, organized, and
-                  productive way of working. With 3rd, your team will achieve
-                  more, together.
-                </p>
-              </Col>
-            </Row>
-          </Container>
-          <Container className="landingImageCont" fluid>
-            <Row className="landingImageRow">
-              <Col className="landingImageCol">
-                <Image
-                  className="LandingImage"
-                  src="https://images.unsplash.com/photo-1577962917302-cd874c4e31d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU5fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                />
-              </Col>
-            </Row>
+            </Container>
+            <Container className="landingImageCont">
+              <Row className="landingImageRow">
+                <Col className="landingImageCol">
+                  <Image
+                    className="LandingImage"
+                    src="https://images.unsplash.com/photo-1521737451536-00a86f630f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjI0fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  />
+                </Col>
+              </Row>
+            </Container>
           </Container>
         </Container>
-
-        <Container className="productDescriptionCont">
-          <Container className="productDescription">
-            <Row>
-              {/* <Col className="productDescriptionIcon">
-              <span className="pdIconCont">
-                <span className="pdIconInner">
-                  <i className="fa-solid fa-comment pdIcon"></i>
-                </span>
-              </span>
-            </Col> */}
-              <Col className="productDescriptionText">
-                <h4>Get the right information, at the right time</h4>
-                <p>
-                  with 3rd's multi-device notifications. Our innovative system
-                  keeps you informed and connected, no matter where you are.
-                  Choose which notifications to receive via email, desktop, or
-                  mobile, and never miss a critical update or task again. With
-                  3rd, you'll have the flexibility to stay informed and
-                  connected, on your own terms. So you can focus on what's
-                  important and achieve more, every day.
-                </p>
-              </Col>
-            </Row>
+        <Container className="ContentContainer landing" fluid>
+          <Container className="productDescriptionCont ">
+            <Container className="descImageCont">
+              <Container className="productDescription">
+                <Col className="productDescriptionText">
+                  <h4>
+                    Whether you're working with a small group or a large team
+                  </h4>
+                  <p>
+                    3rd has the tools you need to streamline communication,
+                    assign tasks, and get things done. Say goodbye to endless
+                    emails and confusion, and hello to a more efficient,
+                    organized, and productive way of working. With 3rd, your
+                    team will achieve more, together.
+                  </p>
+                </Col>
+              </Container>
+              <Container className="landingImageCont">
+                <Row className="landingImageRow">
+                  <Col className="landingImageCol">
+                    <Image
+                      className="LandingImage"
+                      src="https://images.unsplash.com/photo-1577962917302-cd874c4e31d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU5fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    />
+                  </Col>
+                </Row>
+              </Container>
+            </Container>
           </Container>
-          <Container className="landingImageCont" fluid>
-            <Row className="landingImageRow">
-              <Col className="landingImageCol">
-                <Image
-                  className="LandingImage"
-                  src="https://images.unsplash.com/photo-1623303647440-967d26b95b47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzAwfHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                />
-              </Col>
-            </Row>
+        </Container>
+        <Container className="ContentContainer landing" fluid>
+          <Container className="productDescriptionCont ">
+            <Container className="descImageCont">
+              <Container className="productDescription">
+                <Col className="productDescriptionText">
+                  <h4>Get the right information, at the right time</h4>
+                  <p>
+                    with 3rd's multi-device notifications. Our innovative system
+                    keeps you informed and connected, no matter where you are.
+                    Choose which notifications to receive via email, desktop, or
+                    mobile, and never miss a critical update or task again. With
+                    3rd, you'll have the flexibility to stay informed and
+                    connected, on your own terms. So you can focus on what's
+                    important and achieve more, every day.
+                  </p>
+                </Col>
+              </Container>
+              <Container className="landingImageCont">
+                <Row className="landingImageRow">
+                  <Col className="landingImageCol">
+                    <Image
+                      className="LandingImage"
+                      src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dGVhbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    />
+                  </Col>
+                </Row>
+              </Container>
+            </Container>
           </Container>
         </Container>
       </Container>
