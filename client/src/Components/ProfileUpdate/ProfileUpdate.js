0edf423b6b5a985c3fe3a46e822583ddea90125e
Marc S <marcsalaver@gmail.com>
2023-03-14 10:35:57 -0700

    assembled form structure and state handling

--- client/src/Components/ProfileUpdate/ProfileUpdate.js
+++ client/src/Components/ProfileUpdate/ProfileUpdate.js
@@ -2,32 +2,54 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 function ProfileUpdate() {
+  const [formUpdate, setFormUpdate] = useState({
+    username: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+  // need to be able to edit , firstname lastname, username, email, password.
+
+  const handleFormChange = (e) => {
+    setFormUpdate({ ...formUpdate, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container>
-      <Row>
-        <Col>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Username" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicfName">
+                <Form.Label>First name</Form.Label>
+                <Form.Control type="text" placeholder="First name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasiclName">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control type="text" placeholder="Last name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
