aabcc60bcbc384f7d75cb2400137ddd74b68ce65
Marc S <marcsalaver@gmail.com>
2023-03-27 13:34:38 -0700

    setup basic tabs for the settings page.

--- client/src/Pages/Settings/Settings.js
+++ client/src/Pages/Settings/Settings.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { Container, Col, Row, Tab, Nav } from "react-bootstrap";
 import ProfileUpdate from "../../Components/ProfileUpdate/ProfileUpdate";
+import "./Settings.css";
 
 export default function Settings() {
   return (
-    <Container>
-      <Tab.Container defaultActiveKey="profileUpdate">
+    <Container className="settingsCont">
+      <Tab.Container className="" defaultActiveKey="profileUpdate">
         <Row>
-          <Col>
+          <Col className="pstc" sm={3}>
             <Nav varient="pills" className="flex-column">
               <Nav.Item>
                 <Nav.Link eventKey="profileUpdate">Profile</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                <Nav.Link eventKey="Notifications">Tab 2</Nav.Link>
               </Nav.Item>
             </Nav>
           </Col>
-          <Col>
+          <Col sm={9}>
             <Tab.Content>
               <Tab.Pane eventKey="profileUpdate">
                 <ProfileUpdate />
               </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                "This is where another tab should go"
+              <Tab.Pane eventKey="Notifications">
+                "This is where notification updates should go"
               </Tab.Pane>
             </Tab.Content>
           </Col>
