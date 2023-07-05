3f36a0ede872809a3fd3e1175094540ebd7296aa
Marc S <marcsalaver@gmail.com>
2023-07-05 12:31:35 -0700

    adding a character limit for a form

--- client/src/Components/FormModal/Questions/ProductName/ProductName.js
+++ client/src/Components/FormModal/Questions/ProductName/ProductName.js
@@ -2,19 +2,32 @@ import React from "react";
 import { Col, Form } from "react-bootstrap";
 
 export default function ProductName(props) {
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue.length <= 500) {
+      props.handleFormData(event);
+    }
+  };
+
+  const productQuestion1 = props.productValues?.question1 || "";
+
   return (
     <Col>
       <Form.Group className="mb-3" controlId="formTaskTitle">
-        <Form.Label>What is the name product?</Form.Label>
+        <Form.Label>What is the product name?</Form.Label>
         <Form.Control
           name="question1"
-          value={props.productValues.question1}
-          onChange={props.handleFormData}
+          value={productQuestion1}
+          onChange={handleInputChange}
           autoComplete="off"
           required
+          isInvalid={productQuestion1.length > 500}
         />
         <Form.Control.Feedback type="invalid">
-          Please provide a response.
+          {productQuestion1.length > 500 && (
+            <span>Too many characters. Maximum limit is 500.</span>
+          )}
+          {!productQuestion1 && <span>Please provide a response.</span>}
         </Form.Control.Feedback>
       </Form.Group>
     </Col>
